cbde42fe7f1e4eb3eaced93395b6adb44e0f7a4c
oxxo <oxxo.studio@gmail.com>
2018-10-12 11:20:48 +0800

    Use try catch

--- server.js
+++ server.js
@@ -4,33 +4,40 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  
-  let deviceID = req.query.deviceID;  
-  let color = req.query.color;
+
+  let deviceID = req.query.deviceID;
+  let color = req.query.color || '555';
+  let t = req.query.time || 5000;
   console.log(req.query);
-  
-  if( deviceID ){
+
+  if (deviceID) {
     let matrix;
     let a = -1;
-    boardReady({board: 'Bit', device: deviceID, transport: 'mqtt'}, function (board) {
+    let timer;
+    boardReady({ board: 'Bit', device: deviceID, transport: 'mqtt' }, function (board) {
       board.samplingInterval = 250;
       matrix = getMatrix(board, 4, 25);
-      setInterval(()=>{
-        a = a*-1;
-        if(a<0){
-          matrix.setColor('000');
-        }else{
-          matrix.setColor(color);
+      timer = setInterval(() => {
+        try {
+          a = a * -1;
+          if (a < 0) {
+            matrix.setColor('000');
+          } else {
+            matrix.setColor(color);
+          }
+        } catch (error) {
+          clearInterval(timer);
+          console.log('連線異常');
         }
-      },10000);
+      }, t);
     });
-
-    res.status(200).send('Success');
-  }else{
-    res.status(200).send('裝置錯誤');
+    console.log(deviceID + '連線成功，開始閃爍');
+    res.status(200).send(deviceID + '連線成功，開始閃爍');
+  } else {
+    res.status(200).send('裝置錯誤 or 連線異常');
   }
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('伺服器啟動！');
 });
