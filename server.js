cfed5fc5c41e00465f8071b033f270625589cde0
oxxo <oxxo.studio@gmail.com>
2018-10-12 10:43:40 +0800

    Init

---
+++ server.js
@@ -0,0 +1,29 @@
+require('webduino-blockly');
+require('webduino-bit-modules');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  
+  let deviceID = req.query.deviceID;  
+  let color = req.query.color;
+  console.log(req.query);
+  
+  if( deviceID ){
+    var matrix;
+
+    boardReady({board: 'Bit', device: deviceID, transport: 'mqtt'}, function (board) {
+      board.samplingInterval = 250;
+      matrix = getMatrix(board, 4, 25);
+      matrix.setColor(color);
+    });
+
+    res.status(200).send('Success');
+  }else{
+    res.status(200).send('裝置錯誤');
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
